59de92d1658e9427a56401945ab6952e27b31811
gk-j <javvajigokul54@gmail.com>
2026-06-29 12:06:52 -0400

    dropbox initial setup done

---
+++ dropbox/README.md
@@ -0,0 +1,16 @@
+functional requirements:
+    -> user should be able to upload files
+    -> user should be able to download files
+    -> user should be able to share the file 
+
+non-functional requirements:
+    -> availability > consistency
+    -> should be upload large file
+    -> low latency (for uploading,downloding ,syncing)
+    -> secure & reliable
+
+entities:
+    ->files
+    -> file metadata
+    -> users
+
---
+++ dropbox/packages/db/prisma/migrations/20260629155811_init/migration.sql
@@ -0,0 +1,27 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "File" (
+    "id" TEXT NOT NULL,
+    "ownerId" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "mimeType" TEXT NOT NULL,
+    "size" INTEGER NOT NULL,
+    "storageKey" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "File_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "File" ADD CONSTRAINT "File_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
